7d5566d872e83d86693de85bdee71549248f2b2d
Mohammad Hojjat <hojjatjanjan@gmail.com>
2023-02-16 16:03:56 +0430

    create a test file

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Start from "./ant-design/components/login-form";
+import Start from "./ant-design/components/login-form/login-form";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
