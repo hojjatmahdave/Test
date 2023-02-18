beedd0b4690d060d81eba774f0c031d1c5ac82ff
Mohammad Hojjat <hojjatjanjan@gmail.com>
2023-02-18 19:10:17 +0430

    only add my file to index.js

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,12 @@ import Start from "./ant-design/components/login-form/login-form";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import App from "./ant-design/components/test-codes/Test";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <Start />
+    <App />
   </BrowserRouter>
 );
